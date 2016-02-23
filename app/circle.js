d818d6dadaba721523ed905c2b256f97a90ae7ad
Irene Robb <irenerobb72@gmail.com>
2016-02-23 13:31:01 +1300

    circle now moves in a square

--- app/circle.js
+++ app/circle.js
@@ -5,5 +5,12 @@ function drawCircle() {
   circle.x = 300;
   circle.y = 300;
   stage.addChild(circle);
-  stage.update()
+  createjs.Tween.get(circle, {loop: true})
+    .to({x: 400}, 1000, createjs.Ease.getPowInOut(4))
+    .to({alpha: 0, y: 75}, 500, createjs.Ease.getPowInOut(2))
+    .to({alpha: 0, y: 125}, 100)
+    .to({alpha: 1, y: 100}, 500, createjs.Ease.getPowInOut(2))
+    .to({x: 100}, 800, createjs.Ease.getPowInOut(2));
+  createjs.Ticker.setFPS(60)
+  createjs.Ticker.addEventListener('tick', stage)
 }
