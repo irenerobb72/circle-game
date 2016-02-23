26eb28bb6f386f1cf38ba35047c4dc0f3fb16d8e
Irene Robb <irenerobb72@gmail.com>
2016-02-23 17:00:52 +1300

    adding 2nd circle

--- app/circle.js
+++ app/circle.js
@@ -1,12 +1,13 @@
 function drawCircles() {
   var stage = new createjs.Stage('demoCanvas')
-  var circle = new createjs.Shape()
-  circle.graphics.beginFill('crimson')
-  circle.graphics.drawCircle(0, 0, 50)
-  circle.graphics.fillStyle = 'rgba(185, 211, 238'
-  circle.x = 100
-  circle.y = 100
-
-  stage.addChild(circle)
+  var circle1 = new createjs.Shape()
+    circle1.graphics.beginFill('crimson').drawCircle(0, 0, 50)
+    circle1.x = 100
+    circle1.y = 100
+  var circle2 = new createjs.Shape()
+    circle2.graphics.beginFill('blue').drawCircle(0, 0, 50)
+    circle2.x = 200
+    circle2.y = 200
+  stage.addChild(circle1, circle2)
   stage.update()
 }
