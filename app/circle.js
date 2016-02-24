9bf99cc6fd0370b5532a07eb64267040e877b359
Irene Robb <irenerobb72@gmail.com>
2016-02-25 12:26:02 +1300

    cool

--- app/circle.js
+++ app/circle.js
@@ -1,9 +1,25 @@
+var x = 100
+var y = 200
+var dx = 5
+var dy = 5
+
 function init() {
   var mainCanvas = document.getElementById("myCanvas");
   var mainContext = mainCanvas.getContext('2d');
+  setInterval(draw, 10)
+}
+
+function draw() {
+  var mainCanvas = document.getElementById("myCanvas");
+  var mainContext = mainCanvas.getContext('2d');
+  mainContext.clearRect(0, 0, 700, 700)
   mainContext.beginPath()
   mainContext.fillStyle = "#0000FF"
-  mainContext.arc(100, 100, 20, 0, Math.PI*2, true)
+  mainContext.arc(x, y, 20, 0, Math.PI*2, true)
   mainContext.closePath()
   mainContext.fill()
+  if(x < 0 || x > 300) dx=-dx
+  if(y < 0 || y > 300) dy=-dy
+  x+=dx;
+  y+=dy;
 }
