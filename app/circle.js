1c94d8ecf04c976dc0cf1134ecb48be36ea5c2d5
Irene Robb <irenerobb72@gmail.com>
2016-02-24 20:55:22 +1300

    adding junk

--- app/circle.js
+++ app/circle.js
@@ -1,20 +1,7 @@
-import $ from 'jquery'
 
-function blah () {
-  $('document').on('click' function() {
-    var canvas = document.getElementById('myCanvas');
-    var context = canvas.getContext('2d');
-    var centerX = canvas.width / 2;
-    var centerY = canvas.height / 2;
-    var radius = 70;
-
-    context.beginPath();
-    context.arc(centerX, centerY, radius, 0, 2 * Math.PI, false);
-    context.fillStyle = 'green';
-    context.fill();
-    context.lineWidth = 5;
-    context.strokeStyle = '#003300';
-    context.stroke();
-  }
-  blah()
-})
+function hi () {
+  var c = document.getElementById('#myCanvas')
+  c.beginPath()
+  c.arc(95,50,40,0,2*Math.PI)
+  c.stroke()
+}
