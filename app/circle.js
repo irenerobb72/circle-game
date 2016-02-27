1a23fac2329e9ca267f7324323462a79ee523acb
Irene Robb <irenerobb72@gmail.com>
2016-02-27 18:30:14 +1300

    kjk

--- app/circle.js
+++ app/circle.js
@@ -1,22 +1,16 @@
-var mainCanvas = document.getElementById("myCanvas");
-var mainContext = mainCanvas.getContext('2d');
-var circles = new Array();
-
-function init() {
-  var mainCanvas = document.getElementById("myCanvas")
-  var mainContext = mainCanvas.getContext('2d')
-  setInterval(setUpCircles, 10)
-}
+var mainCanvas = document.getElementById("myCanvas")
+var mainContext = mainCanvas.getContext('2d')
+var circles = new Array()
 
 function setupCircles() {
   var mainCanvas = document.getElementById("myCanvas")
   var mainContext = mainCanvas.getContext('2d')
   for (var i = 0; i < 100; i++) {
-    var randomX = Math.round(-200 + Math.random() * 4000);
-    var randomY = Math.round(-200 + Math.random() * 4000);
-    var speed = 0.2 + Math.random() * 9;
-    var size = 5 + Math.random() * 50;
-    var radius = 50 + Math.random() * 100;
+    var randomX = Math.round(-200 + Math.random() * 4000)
+    var randomY = Math.round(-200 + Math.random() * 4000)
+    var speed = 0.2 + Math.random() * 9
+    var size = 5 + Math.random() * 50
+    var radius = 50 + Math.random() * 100
     var circle = new Circle(radius, speed, size, randomX, randomY);
     circles.push(circle);
     mainContext.beginPath();
@@ -25,10 +19,10 @@ function setupCircles() {
                   this.width,
                   0,
                   Math.PI * 3,
-                  false);
-    mainContext.closePath();
+                  false)
+    mainContext.closePath()
     }
-    drawAndUpdate();
+    drawAndUpdate()
   }
 
-setupCircles();
+setupCircles()
