6b8ad00a06445f39579b143f4de35003598d9950
Irene Robb <irenerobb72@gmail.com>
2016-03-17 05:35:51 +1300

    didnt save a file

--- app/circle.js
+++ app/circle.js
@@ -1,3 +1,8 @@
 var mainCanvas = document.getElementById("myCanvas")
 var mainContext = mainCanvas.getContext('2d')
 var circles = new Array()
+
+function createCircles () {
+  var mainCanvas = document.getElementById("myCanvas")
+  var mainContext = mainCanvas.getContext('2d')
+}
