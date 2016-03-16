9cfa2b47138a598044a2cab368a13e6970d4e4a7
Irene Robb <irenerobb72@gmail.com>
2016-03-16 14:47:36 +1300

    yay no error

--- app/circle.js
+++ app/circle.js
@@ -2,6 +2,12 @@ var mainCanvas = document.getElementById("myCanvas")
 var mainContext = mainCanvas.getContext('2d')
 var circles = new Array()
 
+function init() {
+  var mainCanvas = document.getElementById("myCanvas")
+  var mainContext = mainCanvas.getContext('2d')
+  setInterval(setUpCircles, 10)
+}
+
 function setupCircles() {
   var mainCanvas = document.getElementById("myCanvas")
   var mainContext = mainCanvas.getContext('2d')
@@ -9,10 +15,9 @@ function setupCircles() {
     var randomX = Math.round(-200 + Math.random() * 4000)
     var randomY = Math.round(-200 + Math.random() * 4000)
     var speed = 0.2 + Math.random() * 9
+    var size = 5 + Math.random() * 50
     var radius = 50 + Math.random() * 100
-    var circle = new setupCircles(radius, speed, randomX, randomY)
-    circles.push(circle);
-    mainContext.beginPath();
+    mainContext.beginPath()
     mainContext.arc(this.xPos + Math.cos(this.counter / 200) * this.radius,
                   this.yPos + Math.sin(this.counter / 200) * this.radius,
                   this.width,
