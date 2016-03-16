9bbcbf4c12a55007880153305f65dc8a70bf81e2
Irene Robb <irenerobb72@gmail.com>
2016-03-17 05:43:59 +1300

    adding pseudocode for the create circles function. with speed and size

--- app/circle.js
+++ app/circle.js
@@ -5,4 +5,9 @@ var circles = new Array()
 function createCircles () {
   var mainCanvas = document.getElementById("myCanvas")
   var mainContext = mainCanvas.getContext('2d')
+  mainContext.beginPath()
+  var circles = mainContext.arc(Math.random() * (50 - 5) + 5 )
+  var speed = mainContext
+  var radius = mainContext
+  mainContext.closePath()
 }
