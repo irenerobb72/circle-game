f81c1d98aa415f826a13904d08c49328300de681
Irene Robb <irenerobb72@gmail.com>
2016-03-17 15:15:36 -0400

    switching branches

--- app/circle.js
+++ app/circle.js
@@ -1,13 +1,17 @@
-var mainCanvas = document.getElementById("myCanvas")
-var mainContext = mainCanvas.getContext('2d')
-var circles = new Array()
+function init() {
+  var mainCanvas = document.getElementById("myCanvas")
+  var mainContext = mainCanvas.getContext('2d')
+  var circles = new Array()
+}
 
 function createCircles () {
   var mainCanvas = document.getElementById("myCanvas")
   var mainContext = mainCanvas.getContext('2d')
+  mainContext.clearRect(0, 0, 700, 700)
   mainContext.beginPath()
   var circles = mainContext.arc(Math.random() * (50 - 5) + 5 )
   var speed = mainContext
   var radius = mainContext
   mainContext.closePath()
 }
+init()
