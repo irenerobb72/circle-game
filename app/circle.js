69097ce8ee1be250740c309d38a0f2bf8e138c74
Irene Robb <irenerobb72@gmail.com>
2016-03-19 19:39:57 -0400

    no semi colons

--- app/circle.js
+++ app/circle.js
@@ -1,67 +1,67 @@
-var mainCanvas = document.getElementById("myCanvas");
-var mainContext = mainCanvas.getContext('2d');
+var mainCanvas = document.getElementById("myCanvas")
+var mainContext = mainCanvas.getContext('2d')
 
-var circles = [];
+var circles = []
 
 function Circle(radius, speed, width, xPos, yPos) {
-    this.radius = radius;
-    this.speed = speed;
-    this.width = width;
-    this.xPos = xPos;
-    this.yPos = yPos;
-    this.opacity = .05 + Math.random() * .5;
+    this.radius = radius
+    this.speed = speed
+    this.width = width
+    this.xPos = xPos
+    this.yPos = yPos
+    this.opacity = .05 + Math.random() * .5
 
-    this.counter = 0;
+    this.counter = 0
 
-    var signHelper = Math.floor(Math.random() * 2);
+    var signHelper = Math.floor(Math.random() * 2)
 
     if (signHelper == 1) {
-        this.sign = -1;
+        this.sign = -1
     } else {
-        this.sign = 1;
+        this.sign = 1
     }
 }
 
 Circle.prototype.update = function () {
-    this.counter += this.sign * this.speed;
+    this.counter += this.sign * this.speed
 
-    mainContext.beginPath();
+    mainContext.beginPath()
     mainContext.arc(this.xPos + Math.cos(this.counter / 100) * this.radius,
                     this.yPos + Math.sin(this.counter / 100) * this.radius,
                     this.width,
                     0,
                     Math.PI * 2,
-                    false);
+                    false)
 
-    mainContext.closePath();
+    mainContext.closePath()
 
-    mainContext.fillStyle = 'rgba(185, 211, 238,' + this.opacity + ')';
-    mainContext.fill();
+    mainContext.fillStyle = 'rgba(185, 211, 238,' + this.opacity + ')'
+    mainContext.fill()
 };
 
 function setupCircles() {
     for (var i = 0; i < 100; i++) {
-        var randomX = Math.round(-200 + Math.random() * 700);
-        var randomY = Math.round(-200 + Math.random() * 700);
-        var speed = .2 + Math.random() * 3;
-        var size = 5 + Math.random() * 100;
-        var radius = 50 + Math.random() * 100;
+        var randomX = Math.round(-200 + Math.random() * 700)
+        var randomY = Math.round(-200 + Math.random() * 700)
+        var speed = .2 + Math.random() * 3
+        var size = 5 + Math.random() * 100
+        var radius = 50 + Math.random() * 100
 
-        var circle = new Circle(radius, speed, size, randomX, randomY);
-        circles.push(circle);
+        var circle = new Circle(radius, speed, size, randomX, randomY)
+        circles.push(circle)
     }
-    drawAndUpdate();
+    drawAndUpdate()
 }
-setupCircles();
+setupCircles()
 
 function drawAndUpdate() {
-    mainContext.clearRect(0, 0, 500, 500);
+    mainContext.clearRect(0, 0, 500, 500)
 
     for (var i = 0; i < circles.length; i++) {
 
-        var myCircle = circles[i];
-        myCircle.update();
+        var myCircle = circles[i]
+        myCircle.update()
     }
 
-    requestAnimationFrame(drawAndUpdate);
+    requestAnimationFrame(drawAndUpdate)
 }
