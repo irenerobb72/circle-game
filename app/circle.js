93d8b168410b1ccc41fff84dd0509fa2c84d172a
Irene Robb <irenerobb72@gmail.com>
2016-03-19 19:37:32 -0400

    using example again

--- app/circle.js
+++ app/circle.js
@@ -1,24 +1,67 @@
-function init() {
-  var mainCanvas = document.getElementById("myCanvas")
-  var mainContext = mainCanvas.getContext('2d')
-  var circles = new Array()
+var mainCanvas = document.getElementById("myCanvas");
+var mainContext = mainCanvas.getContext('2d');
+
+var circles = [];
+
+function Circle(radius, speed, width, xPos, yPos) {
+    this.radius = radius;
+    this.speed = speed;
+    this.width = width;
+    this.xPos = xPos;
+    this.yPos = yPos;
+    this.opacity = .05 + Math.random() * .5;
+
+    this.counter = 0;
+
+    var signHelper = Math.floor(Math.random() * 2);
+
+    if (signHelper == 1) {
+        this.sign = -1;
+    } else {
+        this.sign = 1;
+    }
 }
 
-function setUpCircles() {
-  for (var i = 0; i < 100; i++){
-    var mainCanvas = document.getElementById("myCanvas")
-    var mainContext = mainCanvas.getContext('2d')
-    mainContext.beginPath()
-    mainContext.fillStyle = '#0000FF'
-    mainContext.closePath()
-    var randomX = Math.round(-200 + Math.random() * 4000)
-    var randomY = Math.round(-200 + Math.random() * 4000)
-    var size = 5 + Math.random() * 50
-    var radius = 50 + Math.random() * 100
+Circle.prototype.update = function () {
+    this.counter += this.sign * this.speed;
 
-    if(randomX < 0 || randomX > 600) size=-size
-    if(randomY < 0 || randomY > 500) size=-radius
-    randomX+=size
-    randomY+=radius
-  }
+    mainContext.beginPath();
+    mainContext.arc(this.xPos + Math.cos(this.counter / 100) * this.radius,
+                    this.yPos + Math.sin(this.counter / 100) * this.radius,
+                    this.width,
+                    0,
+                    Math.PI * 2,
+                    false);
+
+    mainContext.closePath();
+
+    mainContext.fillStyle = 'rgba(185, 211, 238,' + this.opacity + ')';
+    mainContext.fill();
+};
+
+function setupCircles() {
+    for (var i = 0; i < 100; i++) {
+        var randomX = Math.round(-200 + Math.random() * 700);
+        var randomY = Math.round(-200 + Math.random() * 700);
+        var speed = .2 + Math.random() * 3;
+        var size = 5 + Math.random() * 100;
+        var radius = 50 + Math.random() * 100;
+
+        var circle = new Circle(radius, speed, size, randomX, randomY);
+        circles.push(circle);
+    }
+    drawAndUpdate();
+}
+setupCircles();
+
+function drawAndUpdate() {
+    mainContext.clearRect(0, 0, 500, 500);
+
+    for (var i = 0; i < circles.length; i++) {
+
+        var myCircle = circles[i];
+        myCircle.update();
+    }
+
+    requestAnimationFrame(drawAndUpdate);
 }
